03c1facdc4be4ed1fb11cb0f48a6dc743b24bead
ElaineT4 <elaine.tay@ypccollege.edu.my>
2025-08-03 15:02:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -97,6 +97,10 @@ const projectsData = {
       "Logo/Penpals Logo.png",
       "Logo/Penpals Logo2.png"
     ],
+    description: "A logo designed for a stationary brand.",
+    duration: "3 days",
+    tools: ["Illustrator"]
+  },
   "Hustle& Bustle": {
     type: "image",
     imageSrc: "Typography/Typography-1.PNG",
@@ -237,6 +241,7 @@ navLinks.forEach(link => {
     link.classList.add("active");
   }
 });
+
 
 
 
